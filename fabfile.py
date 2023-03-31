f0d5088170e0d76e2fbc2aff9d19f11c3d0a3650
Damian Majsner <d.majsner@gmail.com>
2023-03-31 22:43:34 +0200

    switch from webpack to esbuild

--- fabfile.py
+++ fabfile.py
@@ -7,18 +7,30 @@ from fabric import task
 
 
 @task
-def build(ctx, name='johnny+webapp'):
+def build(ctx, names='johnny+webapp'):
     """Build images"""
-    name = name.replace('+', ' ')
-    ctx.run(f'docker compose build {name}', env=os.environ)
+    names = names.replace('+', ' ')
+    ctx.run(f'docker compose build {names}', env=os.environ)
 
 
 @task
-def run(ctx, name='johnny+webapp+nginx+certbot'):
+def run(ctx, names='johnny+webapp+nginx+certbot'):
     """Start all containers necessary to run the system"""
-    name = name.replace('+', ' ')
+    names = names.replace('+', ' ')
     ctx.run(
-        f'docker compose up -d {name}',
+        f'docker compose up -d {names}',
+        env={
+            'COMPOSE_HTTP_TIMEOUT': '300',
+            'USER_ID': str(os.geteuid()),
+            **os.environ,
+        },
+    )
+
+@task
+def run_dev(ctx, docker='webapp'):
+    """Start container in development mode"""
+    ctx.run(
+        f'docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d {docker}',
         env={
             'COMPOSE_HTTP_TIMEOUT': '300',
             'USER_ID': str(os.geteuid()),
@@ -28,45 +40,45 @@ def run(ctx, name='johnny+webapp+nginx+certbot'):
 
 
 @task
-def stop(ctx, name='johnny+webapp+nginx+certbot'):
+def stop(ctx, names='johnny+webapp+nginx+certbot'):
     """Stop all containers"""
-    name = name.replace('+', ' ')
-    ctx.run(f'docker compose stop {name}', env=os.environ)
+    names = names.replace('+', ' ')
+    ctx.run(f'docker compose stop {names}', env=os.environ)
 
 
 @task
-def restart(ctx, name='johnny+webapp+nginx+certbot'):
+def restart(ctx, names='johnny+webapp+nginx+certbot'):
     """Restart all containers"""
-    name = name.replace('+', ' ')
-    ctx.run(f'docker compose restart {name}', env=os.environ)
+    names = names.replace('+', ' ')
+    ctx.run(f'docker compose restart {names}', env=os.environ)
 
 
 @task
-def purge(ctx, name='johnny+webapp+nginx+certbot'):
+def purge(ctx, names='johnny+webapp+nginx+certbot'):
     """Stop and purge (remove) containers"""
-    name = name.replace('+', ' ')
-    ctx.run(f'docker compose stop {name} && docker compose rm -f {name}', env=os.environ)
+    names = names.replace('+', ' ')
+    ctx.run(f'docker compose stop {names} && docker compose rm -f {names}', env=os.environ)
 
 
 @task
-def sh(ctx, name='johnny'):
+def sh(ctx, docker='johnny'):
     """Spawn and join bash shell in specified node ('johnny' by default)"""
     subprocess.call(['docker compose', 'exec', name, 'env', 'TERM=xterm', '/bin/bash', '-li'])
 
 
 @task
-def zsh(ctx, name='johnny'):
-    """Spawn and join bash shell in specified node ('johnny' by default)"""
+def zsh(ctx, docker='johnny'):
+    """Spawn and join zsh shell in specified node ('johnny' by default)"""
     subprocess.call(['docker', 'compose', 'exec', name, 'env', 'TERM=xterm', '/bin/zsh', '-li'])
 
 
 @task
-def log(ctx, name='johnny'):
-    """Spawn and join bash shell in specified node ('johnny' by default)"""
+def log(ctx, docker='johnny'):
+    """Show logs from specified node ('johnny' by default)"""
     subprocess.call(['docker', 'compose', 'logs', '--tail', 'all', '--follow', name])
 
 
 @task
 def logs(ctx):
-    """Spawn and join bash shell in specified node ('johnny' by default)"""
+    """Show logs from all nodes"""
     subprocess.call(['docker', 'compose', 'logs', '--tail', 'all', '--follow'])
---
+++ webapp/esbuild.config.js
@@ -0,0 +1,90 @@
+import { sassPlugin } from 'esbuild-sass-plugin'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
+import esbuild from 'esbuild'
+import * as fs from 'fs'
+// import manifestPlugin from 'esbuild-plugin-manifest'
+
+const args = process.argv.slice(2)
+const watch = args.includes('--watch')
+
+const DIST_DIR = './dist'
+
+let context = await esbuild
+  .context({
+    entryPoints: ['src/app.tsx'],
+    // watch: args.includes('--watch'),
+    // watch: true,
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    logLevel: 'info',
+    outdir: 'dist',
+    format: 'esm',
+    loader: {
+      '.png': 'file',
+      '.svg': 'file',
+      '.ttf': 'file',
+      '.woff': 'file',
+      '.woff2': 'file',
+      '.eot': 'file',
+    },
+    plugins: [
+      // manifestPlugin(),
+      sassPlugin({
+        async transform(source) {
+          const { css } = await postcss([autoprefixer]).process(source)
+          return css
+        },
+      }),
+    ],
+    define: {
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+    },
+  })
+  .then((context) => {
+    fs.copyFile(`src/index.html`, `${DIST_DIR}/index.html`, (err) => {
+      if (err) throw err
+      console.log(`${DIST_DIR}/index.html: copied.`)
+    })
+    return context
+  })
+
+if (watch) {
+  await context.watch()
+  console.log('watching')
+} else {
+  await context.rebuild()
+  console.log('⚡ Build complete! ⚡')
+  context.dispose()
+}
+
+// TODO
+// new WebpackPwaManifest({
+//     name: 'RaspiJS',
+//     short_name: 'RaspiJS',
+//     description: '',
+//     background_color: '#ffffff',
+//     crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+//     icons: [
+//         {
+//             src: path.resolve('src/assets/images/logo-vertical.png'),
+//             sizes: [96, 128, 192, 256, 384, 512], // multiple sizes
+//         },
+//         {
+//             src: path.resolve('src/assets/images/logo-vertical.png'),
+//             size: '1024x1024', // you can also use the specifications pattern
+//         },
+//     ],
+// }),
+//     new WorkboxPlugin.GenerateSW({
+//         swDest: 'sw.js',
+//         clientsClaim: true,
+//         skipWaiting: true,
+//     }),
