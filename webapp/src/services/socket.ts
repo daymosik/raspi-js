8b3b10ee43277f77acfbf4007e19275cac1aa7f8
Damian Majsner <d.majsner@gmail.com>
2026-04-19 19:09:20 +0200

    update socket port

--- webapp/src/services/socket.ts
+++ webapp/src/services/socket.ts
@@ -1,7 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_ADDRESS = 'https://daymosik.freemyip.com:443'
-// const SOCKET_ADDRESS = 'https://192.168.1.180:443'
+const SOCKET_ADDRESS = 'https://daymosik.freemyip.com:5000'
 
 const socket = io(SOCKET_ADDRESS, {
   path: '/chat/socket.io',
